522ff00e2dbdc277328be6bc94a792b9a8ed9129
Tyler Coffman <tyler.coffman@appfolio.com>
2024-11-21 13:19:41 -0800

    WIP: Harpoon

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -139,4 +139,45 @@ return {
   {
     'tpope/vim-eunuch',
   },
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+    config = function()
+      require('harpoon'):setup()
+    end,
+    keys = function()
+      -- basic telescope configuration
+      local conf = require('telescope.config').values
+      local function toggle_telescope(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        require('telescope.pickers')
+          .new({}, {
+            prompt_title = 'Harpoon',
+            finder = require('telescope.finders').new_table {
+              results = file_paths,
+            },
+            previewer = conf.file_previewer {},
+            sorter = conf.generic_sorter {},
+          })
+          :find()
+      end
+      return {
+        {
+          '<leader>a',
+          function()
+            toggle_telescope(require('harpoon'):list())
+          end,
+          desc = 'Harpoon menu',
+        },
+      }
+    end,
+  },
 }
