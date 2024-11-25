b2689477ecf47ad2d9a108f96c40618bb563cf95
Tyler Coffman <tyler.coffman@appfolio.com>
2024-11-24 16:54:01 -0800

    smart-splits.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -180,4 +180,43 @@ return {
       }
     end,
   },
+  {
+    'mrjones2014/smart-splits.nvim',
+    build = './kitty/install-kittens.bash',
+    lazy = false,
+    keys = {
+      {
+        '<C-h>',
+        function()
+          require('smart-splits').move_cursor_left()
+        end,
+        mode = { 'i', 'n', 'v' },
+        desc = 'Move to left window',
+      },
+      {
+        '<C-j>',
+        function()
+          require('smart-splits').move_cursor_down()
+        end,
+        mode = { 'i', 'n', 'v' },
+        desc = 'Move to bottom window',
+      },
+      {
+        '<C-k>',
+        function()
+          require('smart-splits').move_cursor_up()
+        end,
+        mode = { 'i', 'n', 'v' },
+        desc = 'Move to top window',
+      },
+      {
+        '<C-l>',
+        function()
+          require('smart-splits').move_cursor_right()
+        end,
+        mode = { 'i', 'n', 'v' },
+        desc = 'Move to right window',
+      },
+    },
+  },
 }
