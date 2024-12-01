885b971fac43f153886b5d39a121cbca61687d63
Tyler Coffman <tyler.coffman@appfolio.com>
2024-11-30 23:03:20 -0800

    Change gitsigns keymaps.

--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -33,28 +33,27 @@ return {
         end)
 
         -- Actions
-        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = '[s]tage hunk' })
-        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = '[r]eset hunk' })
-        map('v', '<leader>hs', function()
+        map('n', '<leader>ghs', gitsigns.stage_hunk, { desc = '[s]tage hunk' })
+        map('n', '<leader>ghr', gitsigns.reset_hunk, { desc = '[r]eset hunk' })
+        map('v', '<leader>ghs', function()
           gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
         end, { desc = '[s]tage hunk' })
-        map('v', '<leader>hr', function()
+        map('v', '<leader>ghr', function()
           gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
         end, { desc = '[r]eset hunk' })
-        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = '[S]tage buffer' })
-        map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = '[u]ndo stage hunk' })
-        map('n', '<leader>hR', gitsigns.reset_buffer, { desc = '[R]eset buffer' })
-        map('n', '<leader>hp', gitsigns.preview_hunk, { desc = '[p]review hunk' })
-        map('n', '<leader>hb', function()
+        map('n', '<leader>ghS', gitsigns.stage_buffer, { desc = '[S]tage buffer' })
+        map('n', '<leader>ghu', gitsigns.undo_stage_hunk, { desc = '[u]ndo stage hunk' })
+        map('n', '<leader>ghR', gitsigns.reset_buffer, { desc = '[R]eset buffer' })
+        map('n', '<leader>ghp', gitsigns.preview_hunk, { desc = '[p]review hunk' })
+        map('n', '<leader>ghb', function()
           gitsigns.blame_line { full = true }
         end, { desc = '[b]lame line' })
         map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = 'toggle [b]lame' })
-        map('n', '<leader>hd', gitsigns.diffthis, { desc = '[d]iff' })
-        map('n', '<leader>hD', function()
+        map('n', '<leader>ghd', gitsigns.diffthis, { desc = '[d]iff' })
+        map('n', '<leader>ghD', function()
           gitsigns.diffthis '~'
         end, { desc = '[D]iff against ~' })
         map('n', '<leader>td', gitsigns.toggle_deleted, { desc = 'toggle deleted' })
-
         -- Text object
         map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
       end,
