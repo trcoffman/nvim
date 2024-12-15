2e957dcc7ff0d18043c13cabc85a4d72c0c4a116
Tyler Coffman <tyler.coffman@appfolio.com>
2024-12-15 00:03:45 -0800

    Fix openingh config so that it wouldn't be lazy loaded.

--- lua/custom/plugins/openingh.lua
+++ lua/custom/plugins/openingh.lua
@@ -1,10 +1,9 @@
 return {
   'Almo7aya/openingh.nvim',
   lazy = false,
-  keys = {
-    '<leader>gl',
-    ':OpenInGHFile',
-    mode = { 'n', 'v' },
-    desc = 'Open file in GitHub',
-  },
+  config = function()
+    vim.api.nvim_set_keymap('n', '<leader>gl', '<cmd>OpenInGHFileLines<CR>', { noremap = true, silent = true, desc = 'Open lines in GitHub' })
+    vim.api.nvim_set_keymap('n', '<leader>gf', '<cmd>OpenInGHFile<CR>', { noremap = true, silent = true, desc = 'Open file in GitHub' })
+    vim.api.nvim_set_keymap('n', '<leader>gr', '<cmd>OpenInGHRepo<CR>', { noremap = true, silent = true, desc = 'Open repo in GitHub' })
+  end,
 }
