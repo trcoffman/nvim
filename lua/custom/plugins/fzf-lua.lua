b3436a84ff31884d3c8a129efb2cf1e676cd8e5d
Tyler Coffman <tyler.coffman@appfolio.com>
2025-01-11 20:50:11 -0800

    Switch to fzf-lua from telescope.

---
+++ lua/custom/plugins/fzf-lua.lua
@@ -0,0 +1,29 @@
+return {
+  'ibhagwan/fzf-lua',
+  -- optional for icon support
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    local fzf = require 'fzf-lua'
+    fzf.setup {
+      file = {
+        path_shorten = true,
+      },
+      grep = {
+        path_shorten = true,
+      },
+    }
+
+    -- Keymaps
+    vim.keymap.set('n', '<leader>sc', fzf.commands, { desc = '[S]earch [C]ommands' })
+    vim.keymap.set('n', '<leader>sh', fzf.help_tags, { desc = '[S]earch [H]elp' })
+    vim.keymap.set('n', '<leader>sk', fzf.keymaps, { desc = '[S]earch [K]eymaps' })
+    vim.keymap.set('n', '<leader>sf', fzf.files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>ss', fzf.builtin, { desc = '[S]earch [S]elect fzf-lua' })
+    vim.keymap.set('n', '<leader>sw', fzf.grep_cword, { desc = '[S]earch current [W]ord' })
+    vim.keymap.set('n', '<leader>sg', fzf.live_grep, { desc = '[S]earch by [G]rep' })
+    vim.keymap.set('n', '<leader>sd', fzf.diagnostics_document, { desc = '[S]earch [D]iagnostics' })
+    vim.keymap.set('n', '<leader>sr', fzf.resume, { desc = '[S]earch [R]esume' })
+    vim.keymap.set('n', '<leader>s.', fzf.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+    vim.keymap.set('n', '<leader><leader>', fzf.buffers, { desc = '[ ] Find existing buffers' })
+  end,
+}
