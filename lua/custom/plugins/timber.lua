513a9396a796c489b81932c82705041766abb853
Tyler Coffman <tyler.coffman@appfolio.com>
2025-03-21 11:26:24 -0700

    Log capturing in timber
    You have to run metro bundler like this to get it to work
    
    `script -F /tmp/timber.log yarn start`

--- lua/custom/plugins/timber.lua
+++ lua/custom/plugins/timber.lua
@@ -4,8 +4,8 @@ return {
   version = '*', -- Use for stability; omit to use `main` branch for the latest features
   event = 'VeryLazy',
   config = function()
-    local jsTemplate = [[console.log('>>>>> %filename:%line_number ', '%log_target', %log_target);]]
-    local jsBatch = [[console.log('>>>>> %filename:%line_number ', { %repeat<%log_target><, > });]]
+    local jsTemplate = [[console.log('%watcher_marker_start %filename:%line_number ', '%log_target', %log_target, '%watcher_marker_end');]]
+    local jsBatch = [[console.log('%watcher_marker_start %filename:%line_number ', { %repeat<%log_target><, > }, '%watcher_marker_end');]]
     require('timber').setup {
       log_templates = {
         default = {
@@ -23,6 +23,35 @@ return {
           tsx = jsBatch,
         },
       },
+      log_watcher = {
+        enabled = true,
+        sources = {
+          javascript_log = {
+            type = 'filesystem',
+            name = 'Log File',
+            path = '/tmp/timber.log',
+            buffer = {
+              syntax = 'javascript',
+            },
+          },
+        },
+      },
     }
   end,
+  keys = {
+    {
+      '<leader>lf',
+      function()
+        require('timber.buffers').open_float()
+      end,
+      desc = 'Open Timber Float',
+    },
+    {
+      '<leader>ls',
+      function()
+        require('timber.summary').open()
+      end,
+      desc = 'Open Timber Float',
+    },
+  },
 }
