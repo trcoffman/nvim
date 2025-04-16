ae8a307e6c9433461655ff879e29b593c2821261
Tyler Coffman <tyler.coffman@appfolio.com>
2025-04-16 09:57:03 -0700

    Disable neorg

--- lua/custom/plugins/neorg.lua
+++ lua/custom/plugins/neorg.lua
@@ -3,4 +3,5 @@ return {
   lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
   version = '*', -- Pin Neorg to the latest stable release
   config = true,
+  enabled = false,
 }
