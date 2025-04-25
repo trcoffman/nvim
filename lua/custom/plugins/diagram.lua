7e3622ccda3b6ee486a9cbda4af8f4ec7bd12ff3
Tyler Coffman <tyler.coffman@appfolio.com>
2025-04-25 11:51:10 -0700

    diagram.nvim

---
+++ lua/custom/plugins/diagram.lua
@@ -0,0 +1,20 @@
+return {
+  '3rd/diagram.nvim',
+  dependencies = {
+    '3rd/image.nvim',
+  },
+  config = function()
+    require('diagram').setup {
+      integrations = {
+        require 'diagram.integrations.markdown',
+      },
+      renderer_options = {
+        mermaid = {
+          theme = 'forest',
+          background = 'white',
+          scale = 3,
+        },
+      },
+    }
+  end,
+}
