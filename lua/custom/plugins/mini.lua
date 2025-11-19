bdf2cb013c7637d6c43e04e10e02c0e1a8fdad67
Tyler Coffman <tyler.coffman@appfolio.com>
2025-11-19 11:07:12 -0800

    Change mini-surround mappings

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -15,7 +15,19 @@ return { -- Collection of various small independent plugins/modules
     -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
     -- - sd'   - [S]urround [D]elete [']quotes
     -- - sr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup()
+    require('mini.surround').setup {
+      mappings = {
+        add = 'gsa', -- Add surrounding in Normal and Visual modes
+        delete = 'gsd', -- Delete surrounding
+        find = 'gsf', -- Find surrounding (to the right)
+        find_left = 'gsF', -- Find surrounding (to the left)
+        highlight = 'gsh', -- Highlight surrounding
+        replace = 'gsr', -- Replace surrounding
+
+        suffix_last = 'l', -- Suffix to search with "prev" method
+        suffix_next = 'n', -- Suffix to search with "next" method
+      },
+    }
 
     require('mini.diff').setup()
 
