0a1c7f72329801c656f61fe58efe0c8dfb6e8201
Tyler Coffman <tyler.coffman@appfolio.com>
2026-06-04 14:09:56 -0700

    Escape key terminal

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -23,6 +23,13 @@ end, { desc = '[T]oggle [D]iagnostics' })
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+-- Send literal escape to terminal; C-[ no longer works in nvim 0.11+ due to Esc equivalence
+vim.keymap.set('t', '<C-[>', function()
+  local chan = vim.bo.channel
+  if chan ~= 0 then
+    vim.api.nvim_chan_send(chan, '\27')
+  end
+end, { desc = 'Send literal escape to terminal' })
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
